26c0f3c5900f1164a0a4afac5d22f87c349e4cdf
rodrigueclemencet-lang <rodrigueclemencet@gmail.com>
2025-12-19 09:53:30 +0000

    premier commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std;
+int main() {  
+  //variable permettant de compter le nombre de places disponibles  
+  int nbrePlaces = 10;  
+  //variable permettant de récupérer le choix de l’utilisateur  
+  int choix;  cout << "************ Evaluation 2 ************" << endl << endl;
+  int choix;  cout << "1 - Entrée dans le parking" << endl << endl;
+  int choix;  cout << "2 - Nombre de places disponibles" << endl << endl;
+  int choix;  cout << "3 - Sortie de parking" << endl << endl;
+  int choix;  cout << "4 - Quitter" << endl << endl;
+
+
+
+
+
+  return 0;
+}
