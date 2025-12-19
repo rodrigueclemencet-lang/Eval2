846d746f21963fcfef0ecc4857dd61107bd8bf18
rodrigueclemencet-lang <rodrigueclemencet@gmail.com>
2025-12-19 10:44:06 +0000

    ajout menu

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ bool choixValide = false;
 
 
   //variable permettant de compter le nombre de places disponibles  
-  int nbrePlaces = 10;  
+  int nbrePlaces = 10;
   //variable permettant de récupérer le choix de l’utilisateur  
   do {
   cout << "************ Evaluation 2 ************" << endl << endl;
@@ -24,23 +24,22 @@ bool choixValide = false;
 
     if (choixUtilisateur == '1') {
       std::cout << "Entrée dans le parking\n";
+      --nbrePlaces;
       choixValide = true;
     } else if (choixUtilisateur == '2') {
       std::cout << "Nombre de places dans le parking\n";
+      std::cout << nbrePlaces << endl;
       choixValide = true;
     } else if (choixUtilisateur == '3') {
       std::cout << "Sortie de Parking\n";
+      ++nbrePlaces;
       choixValide = true;
     } else if (choixUtilisateur == '4') {
       std::cout << "Quitter\n";
       choixValide = true;
+      std::cout << "Au revoir\n";
     } else {
       std::cout << "Choix invalide\n\n";
     }} while ((choixUtilisateur != '0') && (choixUtilisateur != '4'));
-
-
-
-
-
   return 0;
 }
