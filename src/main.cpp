65a189675016ea2c7c74d59aac2b06b0766b0ebc
rodrigueclemencet-lang <rodrigueclemencet@gmail.com>
2025-12-19 10:10:20 +0000

    ajout menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream> 
 using namespace std;
+unsigned char choixUtilisateur;
+bool choixValide = false;
+
+
 int main() {  
   //variable permettant de compter le nombre de places disponibles  
   int nbrePlaces = 10;  
@@ -10,6 +14,28 @@ int main() {
   cout << "3 - Sortie de parking" << endl << endl;
   cout << "4 - Quitter" << endl << endl;
 
+    std::cout << "****************************************\n";
+
+    std::cout << "Faire votre choix\n";
+    std::cin >> choixUtilisateur;
+    std::cout << "vous avez choisi " << choixUtilisateur << std::endl;
+
+    if (choixUtilisateur == '1') {
+      std::cout << "Entrée dans le parking\n";
+      choixValide = true;
+    } else if (choixUtilisateur == '2') {
+      std::cout << "Nombre de places dans le parking\n";
+      choixValide = true;
+    } else if (choixUtilisateur == '3') {
+      std::cout << "Sortie de Parking\n";
+      choixValide = true;
+    } else if (choixUtilisateur == '4') {
+      std::cout << "Quitter\n";
+      choixValide = true;
+    } else {
+      std::cout << "Choix invalide\n\n";
+    }
+  
 
 
 
