149da0f823decdd98b1acd324243f127647034d2
rodrigueclemencet-lang <rodrigueclemencet@gmail.com>
2025-12-19 10:04:21 +0000

    ajout du menu

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@ int main() {
   //variable permettant de compter le nombre de places disponibles  
   int nbrePlaces = 10;  
   //variable permettant de récupérer le choix de l’utilisateur  
-  int choix;  cout << "************ Evaluation 2 ************" << endl << endl;
-  int choix;  cout << "1 - Entrée dans le parking" << endl << endl;
-  int choix;  cout << "2 - Nombre de places disponibles" << endl << endl;
-  int choix;  cout << "3 - Sortie de parking" << endl << endl;
-  int choix;  cout << "4 - Quitter" << endl << endl;
+  cout << "************ Evaluation 2 ************" << endl << endl;
+  cout << "1 - Entrée dans le parking" << endl << endl;
+  cout << "2 - Nombre de places disponibles" << endl << endl;
+  cout << "3 - Sortie de parking" << endl << endl;
+  cout << "4 - Quitter" << endl << endl;
 
 
 
